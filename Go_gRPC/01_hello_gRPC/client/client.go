9f591f063b34dc4acb46b2a1a462f96fef24527e
kueunsu <sss5732@naver.com>
2025-11-30 23:56:23 +0900

    01_finish

--- Go_gRPC/01_hello_gRPC/client/client.go
+++ Go_gRPC/01_hello_gRPC/client/client.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"log"
 
+	pb "hello_grpc_example/hellopb" // proto에서 생성된 패키지 추가
+
 	"google.golang.org/grpc"
 )
 
@@ -18,12 +20,12 @@ func main() {
 	defer conn.Close()
 
 	// (4) protoc가 생성한 *_pb2_grpc.go 화일의 stub 함수를, (3)의 채널을 사용하여 실행하여 stub를 생성함
-	// 같은 폴더 안에 hello_grpc_pb2_grpc.go 가 있어서, 그냥 NewMyServiceClient 를 바로 쓸 수 있음
-	stub := NewMyServiceClient(conn)
+	// Go에선 hellopb 패키지 안에 있으니 pb.NewMyServiceClient 로 호출
+	stub := pb.NewMyServiceClient(conn) // pb 추가
 
 	// (5) protoc가 생성한 *_pb2.go 화일의 메세지 타입에 맞춰서, 원격 함수에 전달할 메시지를 만들고, 전달할 값을 저장함
-	// hello_grpc_pb2.go 안에 정의된 MyNumber 타입을 그대로 사용
-	request := &MyNumber{Value: 4}
+	// hello_grpc.pb.go 안에 정의된 MyNumber 타입을 그대로 사용
+	request := &pb.MyNumber{Value: 4} // pb 추가
 
 	// (6) 원격 함수를 stub을 사용하여 호출함
 	response, err := stub.MyFunction(context.Background(), request)
